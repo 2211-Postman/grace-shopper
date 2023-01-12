1ce9f3328f0dbddf1a0b7b76ea96a640cf9108fd
Sam N Ting <samnlting@gmail.com>
2023-01-12 18:02:06 -0500

    saving b4 merge with main

--- client/features/cart/cartSlice.js
+++ client/features/cart/cartSlice.js
@@ -3,48 +3,29 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    cart: [],
+    products: [],
+    quantity: 0,
+    total: 0,
   },
   reducers: {
     addToCart: (state, action) => {
-      const productInCart = state.cart.find(
-        (product) => product.id === action.payload.id
-      );
-      if (productInCart) {
-        productInCart.quantity++;
-      } else {
-        state.cart.push({ ...action.payload, quantity: 1 });
-      }
+      state.quantity += 1;
+      state.products.push(action.payload);
+      state.total += action.payload.price * action.payload.quantity;
     },
-    incrementQuantity: (state, action) => {
-      const product = state.cart.find(
-        (product) => product.id === action.payload
-      );
-      product.quantity++;
-    },
-    decrementQuantity: (state, action) => {
-      const product = state.cart.find(
-        (product) => product.id === action.payload
-      );
-      if (product.quantity === 1) {
-        product.quantity = 1;
-      } else {
-        product.quantity--;
-      }
-    },
+
     removeproduct: (state, action) => {
-      const removeproduct = state.cart.filter(
+      const removeproduct = state.products.filter(
         (product) => product.id !== action.payload
       );
-      state.cart = removeproduct;
+      state.products = removeproduct;
     },
   },
 });
 
+export const selectCart = (state) => {
+  return state.cart;
+};
+
 export const cartReducer = cartSlice.reducer;
-export const {
-  addToCart,
-  incrementQuantity,
-  decrementQuantity,
-  removeproduct,
-} = cartSlice.actions;
+export const { addToCart, removeproduct } = cartSlice.actions;
