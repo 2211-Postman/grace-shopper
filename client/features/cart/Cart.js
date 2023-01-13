178a439eb07539e435ecf96f2b37808bc7bec27d
Robert West <robert.david.west@gmail.com>
2023-01-13 15:59:37 -0500

    removed useEffect

--- client/features/cart/Cart.js
+++ client/features/cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Grid, Typography, Container } from "@mui/material";
@@ -13,7 +13,6 @@ const Cart = () => {
   const dispatch = useDispatch();
 
   const cart = useSelector(selectCart);
-  useEffect(() => {}, [cart]);
 
   function checkoutOnClick() {
     navigate(`/checkout`);
