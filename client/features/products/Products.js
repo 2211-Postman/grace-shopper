65f30e586bb932394edf35fdf13ce21dd3401007
Warren Chan <warren.chan0113@gmail.com>
2023-01-15 17:50:47 -0500

    Added View button in card

--- client/features/products/Products.js
+++ client/features/products/Products.js
@@ -1,12 +1,24 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { Grid, Typography, Container } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  Container,
+  Button,
+  FormControlLabel,
+  FormControl,
+  FormGroup,
+  FormLabel,
+  TextField,
+  InputLabel,
+  Input,
+} from "@mui/material";
 import { fetchAllProductsAsync, selectUniqueProducts } from "./productsSlice";
 import ProductsCard from "./ProductsCard";
-import Button from "@mui/material/Button";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { deleteProductAsync } from "./productsSlice";
+import CreateProduct from "./CreateProduct";
 
 const Products = () => {
   const navigate = useNavigate();
@@ -35,7 +47,7 @@ const Products = () => {
 
   return (
     <Container>
-      <Grid container spacing={3} columns={8}>
+      <Grid container spacing={2} columns={8}>
         {products && products.length
           ? products.map((product) => (
               <Grid item xs={8} sm={4} md={2} key={product.id}>
@@ -44,22 +56,31 @@ const Products = () => {
                   onClick={onClick}
                   isLoggedIn={isLoggedIn}
                   isAdmin={isAdmin}
+                  handleDelete={handleDelete}
                 />
-                {isLoggedIn && isAdmin ? (
-                  <Button
-                    variant="outlined"
-                    startIcon={<DeleteIcon />}
-                    onClick={handleDelete(product.id)}
-                  >
-                    Delete
-                  </Button>
-                ) : null}
               </Grid>
             ))
           : null}
+
+        {/* {isLoggedIn && isAdmin ? <CreateProduct /> : null} */}
       </Grid>
     </Container>
   );
 };
 
 export default Products;
+
+// <form action="/somewhere.html">
+//   <FormGroup sx={{padding: 2, borderRadius: 2, border: '1px solid', borderColor: 'primary.main'}}>
+//   <TextField sx={{paddingBottom: 2}} name="specs" variant="outlined" placeholder="Specs..." />
+//   <FormLabel component="legend">Product</FormLabel>
+//   <FormGroup row sx={{paddingBottom: 2}}>
+//     <FormControlLabel control={<Checkbox name="laptop" value="yes" />} label="Laptop" />
+//     <FormControlLabel
+//       control={<Checkbox name="headset" value="yes" />}
+//       label="Head Set"
+//     />
+//   </FormGroup>
+//   <Button type="submit" variant="outlined">Submit</Button>
+//   </FormGroup>
+// </form>
--- client/features/products/ProductsCard.js
+++ client/features/products/ProductsCard.js
@@ -1,41 +1,65 @@
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardActionArea,
+  CardActions,
+  Button,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { usdFormatter } from "../../helpers";
 
-export default function ProductsCard({ product, onClick }) {
+export default function ProductsCard({
+  product,
+  onClick,
+  handleDelete,
+  isLoggedIn,
+  isAdmin,
+}) {
   const { id, productName, brand, price, imageURL } = product;
 
   return (
-    <Card sx={{ display: "flex", flexDirection: "column" }}>
-      <CardActionArea
+    <Card sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
+      {/* <CardActionArea
         sx={{
           display: "flex",
           flexDirection: "column",
         }}
-        onClick={(e) => onClick(id)}
-      >
-        <CardMedia
-          component="img"
-          sx={{ width: 200 }}
-          image={imageURL[0]}
-          alt="Sneaker Photo"
-        />
-        <CardContent sx={{ flex: "1 0 auto" }}>
-          <Typography component="div" variant="p" color="grey">
-            {brand}
-          </Typography>
-          <Typography component="div" variant="p">
-            {productName}
-          </Typography>
-          <Typography component="div" variant="p" color="grey">
-            {`${usdFormatter.format(price)}`}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      > */}
+      <CardMedia
+        component="img"
+        sx={{ width: 200 }}
+        image={imageURL[0]}
+        alt="Sneaker Photo"
+      />
+      <CardContent sx={{ flex: "1 0 auto" }}>
+        <Typography component="div" variant="p" color="grey">
+          {brand}
+        </Typography>
+        <Typography component="div" variant="p">
+          {productName}
+        </Typography>
+        <Typography component="div" variant="p" color="grey">
+          {`${usdFormatter.format(price)}`}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" onClick={(e) => onClick(id)}>
+          View
+        </Button>
+        {isLoggedIn && isAdmin ? (
+          <div>
+            <Button size="small">Edit</Button>
+            <Button
+              startIcon={<DeleteIcon />}
+              onClick={handleDelete(product.id)}
+            ></Button>
+          </div>
+        ) : null}
+      </CardActions>
+      {/* </CardActionArea> */}
     </Card>
   );
 }
