75b96d985d5c0774a997a95d483a7fcfe8faf8e0
Robert West <robert.david.west@gmail.com>
2023-01-17 18:46:49 -0500

    resolved conflict in user route. Added gatekeeper middleware to users, products routes. Orders will be next

--- server/api/users.js
+++ server/api/users.js
@@ -2,13 +2,15 @@ const router = require("express").Router();
 const {
   models: { User, Order },
 } = require("../db");
+const { requireToken, isAdmin, isAdminOrSelf } = require("../gatekeeper");
 
 module.exports = router;
 
-router.get("/", async (req, res, next) => {
+router.get("/", requireToken, isAdmin, async (req, res, next) => {
+  // This route is for admin's to get all user account info
   try {
     const users = await User.findAll({
-      // explicitly select only the id and username fields - even though
+      // explicitly select only the fields we want - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
       attributes: ["id", "firstName", "lastName", "email", "isAdmin"],
@@ -19,7 +21,9 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.get("/:userId", async (req, res, next) => {
+router.get("/:userId", requireToken, isAdmin, async (req, res, next) => {
+  // This route is for admin's to get user account info
+  // non admins would get their own user account info with "auth/me"
   try {
     const user = await User.findByPk(req.params.userId, {
       attributes: ["id", "email"],
@@ -31,26 +35,35 @@ router.get("/:userId", async (req, res, next) => {
   }
 });
 
-router.get("/:userId/orders/", async (req, res, next) => {
+router.get(
+  "/:userId/orders/",
+  requireToken,
+  isAdminOrSelf,
+  async (req, res, next) => {
+    try {
+      const orders = await Order.findAll({
+        where: { userId: req.params.userId },
+      });
+      res.json(orders);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.post("/", requireToken, isAdmin, async (req, res, next) => {
+  // This route is for admin's to create user accounts
+  // non admins would create their own user accounts with "auth/signup"
   try {
-    const orders = await Order.findAll({
-      where: { userId: req.params.userId },
-    });
-    res.json(orders);
+    const { email, password, firstName, lastName } = req.body;
+    const user = await User.create({ email, password, firstName, lastName });
+    res.json(user);
   } catch (err) {
     next(err);
   }
 });
 
-router.post("/", async (req, res, next) => {
-  try {
-    res.json(await User.create(req.body));
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.put("/:userId", async (req, res, next) => {
+router.put("/:userId", requireToken, isAdminOrSelf, async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.userId);
     res.json(await user.update(req.body));
@@ -59,12 +72,17 @@ router.put("/:userId", async (req, res, next) => {
   }
 });
 
-router.delete("/:userId", async (req, res, next) => {
-  try {
-    const user = await User.findByPk(req.params.userId);
-    await user.destroy();
-    res.json(user);
-  } catch (err) {
-    next(err);
+router.delete(
+  "/:userId",
+  requireToken,
+  isAdminOrSelf,
+  async (req, res, next) => {
+    try {
+      const user = await User.findByPk(req.params.userId);
+      await user.destroy();
+      res.json(user);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
